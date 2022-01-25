2abc43790bf7c925cb4891f98e11ec7856673454
sean <seankennett@gmail.com>
2022-01-25 20:26:50 +0000

    Azure AD B2C working so know my user input now

--- VideoDatabase/Tables/UserLayers.sql
+++ VideoDatabase/Tables/UserLayers.sql
@@ -1,13 +1,12 @@
 ﻿CREATE TABLE [dbo].[UserLayers]
 (
 	[UserLayerId] INT IDENTITY NOT NULL PRIMARY KEY,
-	[UserId] INT NOT NULL,
+	[UserObjectId] UNIQUEIDENTIFIER NOT NULL,
 	[LayerId] INT NOT NULL,
 	[UserLayerStatusId] TINYINT NOT NULL,
 	[DateCreated] DATETIME2 NOT NULL,
 	[DateUpdated] DATETIME2 NOT NULL,
 	CONSTRAINT FK_UserLayers_Layer FOREIGN KEY ([LayerId]) REFERENCES [dbo].[Layer]([LayerId]),
-	CONSTRAINT FK_UserLayers_User FOREIGN KEY ([UserId]) REFERENCES [dbo].[User]([UserId]),
 	CONSTRAINT FK_UserLayers_UserLayerStatus FOREIGN KEY ([UserLayerStatusId]) REFERENCES [dbo].[UserLayerStatus]([UserLayerStatusId]),
-	CONSTRAINT UQ_UserLayers_UserId_LayerId UNIQUE ([UserId], [LayerId])
+	CONSTRAINT UQ_UserLayers_UserId_LayerId UNIQUE ([UserObjectId], [LayerId])
 )
