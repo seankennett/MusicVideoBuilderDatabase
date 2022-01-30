998fa5c51e3c2375ce498b96f52d128a664f7c43
sean <seankennett@gmail.com>
2022-01-30 00:33:13 +0000

    GUID changes and better limits

--- VideoDatabase/Tables/LayerTags.sql
+++ VideoDatabase/Tables/LayerTags.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[LayerTags]
 (
 	[LayerTagId] INT IDENTITY NOT NULL PRIMARY KEY,
-	[LayerId] INT NOT NULL,
+	[LayerId] UNIQUEIDENTIFIER NOT NULL,
 	[TagId] INT NOT NULL,
 	CONSTRAINT FK_LayerTags_Layer FOREIGN KEY ([LayerId]) REFERENCES [dbo].[Layer]([LayerId]),
 	CONSTRAINT FK_LayerTags_Tag FOREIGN KEY ([TagId]) REFERENCES [dbo].[Tag]([TagId]),
