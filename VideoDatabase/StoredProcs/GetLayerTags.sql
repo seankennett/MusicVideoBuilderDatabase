850aa07fb67cd8dd1841603a980c738c9c87738e
sean <seankennett@gmail.com>
2022-04-03 21:43:06 +0100

    It's actually getting layer tags so structure change and rename

---
+++ VideoDatabase/StoredProcs/GetLayerTags.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetLayerTags]
+(
+@userObjectId UNIQUEIDENTIFIER
+)
+AS
+
+SELECT [TagId], [TagName] FROM [dbo].[Tag]
+
+SELECT lt.[LayerId], lt.[TagId], l.[LayerName], l.[LayerTypeId], l.[DateUpdated], ul.[UserLayerStatusId]  FROM [dbo].[LayerTags] lt 
+INNER JOIN [dbo].[Layer] l ON l.[LayerId] = lt.[LayerId] 
+LEFT JOIN (SELECT [UserLayerStatusId], [LayerId] FROM [dbo].[UserLayers] WHERE [UserObjectId] = @userObjectId) ul ON l.[LayerId] = ul.[LayerId]
