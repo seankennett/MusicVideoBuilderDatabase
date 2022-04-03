8b9a664fd35a562aeb77390f41a4dd878ab0876c
sean <seankennett@gmail.com>
2022-04-03 15:41:33 +0100

    Adding GetLayers for Layer Finder

---
+++ VideoDatabase/StoredProcs/GetLayers.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetLayers]
+(
+@userObjectId UNIQUEIDENTIFIER
+)
+AS
+	
+SELECT l.[LayerId], l.[LayerTypeId], l.[DateUpdated], ul.UserLayerStatusId  FROM [dbo].[Layer] l LEFT JOIN (SELECT * FROM [dbo].[UserLayers] WHERE UserObjectId = @userObjectId) ul ON l.LayerId = ul.LayerId
+
+SELECT lt.LayerId, t.TagId, t.TagName FROM [dbo].[LayerTags] lt JOIN [dbo].[Tag] t ON lt.TagId = t.TagId
