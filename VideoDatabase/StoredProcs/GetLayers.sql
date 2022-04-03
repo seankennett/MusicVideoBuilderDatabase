133ca079d81eb0eab7d7098fccaa75b23745f4cb
sean <seankennett@gmail.com>
2022-04-03 21:10:12 +0100

    Forgot LayerName

--- VideoDatabase/StoredProcs/GetLayers.sql
+++ VideoDatabase/StoredProcs/GetLayers.sql
@@ -4,6 +4,6 @@
 )
 AS
 	
-SELECT l.[LayerId], l.[LayerTypeId], l.[DateUpdated], ul.UserLayerStatusId  FROM [dbo].[Layer] l LEFT JOIN (SELECT * FROM [dbo].[UserLayers] WHERE UserObjectId = @userObjectId) ul ON l.LayerId = ul.LayerId
+SELECT l.[LayerId], l.[LayerName], l.[LayerTypeId], l.[DateUpdated], ul.UserLayerStatusId  FROM [dbo].[Layer] l LEFT JOIN (SELECT * FROM [dbo].[UserLayers] WHERE UserObjectId = @userObjectId) ul ON l.LayerId = ul.LayerId
 
 SELECT lt.LayerId, t.TagId, t.TagName FROM [dbo].[LayerTags] lt JOIN [dbo].[Tag] t ON lt.TagId = t.TagId
