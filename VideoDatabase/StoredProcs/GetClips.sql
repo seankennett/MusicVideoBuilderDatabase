b434d0261467b25a7e0df542216bcfa3684c64d9
sean <seankennett@gmail.com>
2022-07-16 23:22:34 +0100

    Adding get all clips

---
+++ VideoDatabase/StoredProcs/GetClips.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetClips](
+@userObjectId UNIQUEIDENTIFIER
+)
+AS
+
+SELECT DISTINCT c.ClipId, c.ClipName, c.DateUpdated FROM [Clip] c 
+JOIN [ClipUserLayers] cu ON c.ClipId = cu.ClipId
+JOIN [UserLayer] u ON cu.UserLayerId = u.UserLayerId
+WHERE u.UserObjectId = @userObjectId AND u.UserLayerStatusId > 1
+
+SELECT cu.ClipId, cu.UserLayerId, cu.[Order] FROM [dbo].[ClipUserLayers] cu
+JOIN [dbo].[UserLayer] u ON cu.UserLayerId = u.UserLayerId
+WHERE u.UserObjectId = @userObjectId AND u.UserLayerStatusId > 1
