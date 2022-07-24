c2a80e344ab5838f64cbced33fd7f3d8f5d5484b
sean <seankennett@gmail.com>
2022-07-24 20:39:59 +0100

    Returning layerId for getclips

--- VideoDatabase/StoredProcs/GetClips.sql
+++ VideoDatabase/StoredProcs/GetClips.sql
@@ -8,6 +8,6 @@ JOIN [ClipUserLayers] cu ON c.ClipId = cu.ClipId
 JOIN [UserLayer] u ON cu.UserLayerId = u.UserLayerId
 WHERE u.UserObjectId = @userObjectId AND u.UserLayerStatusId > 1
 
-SELECT cu.ClipId, cu.UserLayerId, cu.[Order] FROM [dbo].[ClipUserLayers] cu
+SELECT cu.ClipId, cu.UserLayerId, cu.[Order], u.LayerId FROM [dbo].[ClipUserLayers] cu
 JOIN [dbo].[UserLayer] u ON cu.UserLayerId = u.UserLayerId
 WHERE u.UserObjectId = @userObjectId AND u.UserLayerStatusId > 1
