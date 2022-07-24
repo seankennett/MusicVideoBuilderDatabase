7e9355d704cb17165d108afdb9a100f9ef3d7a20
sean <seankennett@gmail.com>
2022-07-24 21:00:18 +0100

    Adding singular getclip

---
+++ VideoDatabase/StoredProcs/GetClip.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetClip](
+@userObjectId UNIQUEIDENTIFIER,
+@ClipId INTEGER
+)
+AS
+
+SELECT DISTINCT c.ClipId, c.ClipName, c.DateUpdated FROM [Clip] c 
+JOIN [ClipUserLayers] cu ON c.ClipId = cu.ClipId
+JOIN [UserLayer] u ON cu.UserLayerId = u.UserLayerId
+WHERE c.ClipId = @ClipId AND u.UserObjectId = @userObjectId AND u.UserLayerStatusId > 1
+
+SELECT cu.ClipId, cu.UserLayerId, cu.[Order], u.LayerId FROM [dbo].[ClipUserLayers] cu
+JOIN [dbo].[UserLayer] u ON cu.UserLayerId = u.UserLayerId
+WHERE [cu].[ClipId] = @ClipId AND u.UserObjectId = @userObjectId AND u.UserLayerStatusId > 1
