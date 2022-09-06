fbf769c21d9019bf8bae1bac0be8961431ba36f4
sean <seankennett@gmail.com>
2022-09-06 21:50:09 +0100

    Adding usercount to layerfinders for most popular layes

--- VideoDatabase/StoredProcs/GetLayerFinders.sql
+++ VideoDatabase/StoredProcs/GetLayerFinders.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[GetLayerFinders]
 AS
 
-SELECT [LayerId], [LayerName], [LayerTypeId], [DateUpdated] FROM [dbo].[Layer]
+SELECT [LayerId], [LayerName], [LayerTypeId], [DateUpdated], (SELECT COUNT(*) FROM [dbo].[UserLayer] ul WHERE ul.LayerId = l.LayerId) as UserCount FROM [dbo].[Layer] l
 
 SELECT lt.LayerId, t.TagName FROM [dbo].[LayerTags] lt JOIN [dbo].[Tag] t ON lt.TagId = t.TagId
