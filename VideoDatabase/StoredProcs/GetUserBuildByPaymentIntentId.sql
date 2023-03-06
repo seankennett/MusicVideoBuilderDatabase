cff2d614f51f462d9f0c039400093acfb591d40b
Sean Kennett <seankennettwork@gmail.com>
2023-03-06 22:33:35 +0000

    Added get by paymentintent proc

---
+++ VideoDatabase/StoredProcs/GetUserBuildByPaymentIntentId.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[GetUserBuildByPaymentIntentId](
+@PaymentIntentId VARCHAR(30)
+)
+AS
+
+SELECT BuildId, BuildStatusId, LicenseId, ResolutionId, VideoId, PaymentIntentId, HasAudio, DateUpdated, UserObjectId  FROM [dbo].[Build] WHERE PaymentIntentId = @PaymentIntentId
