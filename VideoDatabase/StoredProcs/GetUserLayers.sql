b123c7c1ec1a848d00af61d1b4356aff8450bf28
Sean Kennett <seankennettwork@gmail.com>
2023-03-17 19:59:16 +0000

    Forgetting status on userlayer

--- VideoDatabase/StoredProcs/GetUserLayers.sql
+++ VideoDatabase/StoredProcs/GetUserLayers.sql
@@ -1,10 +1,9 @@
-﻿CREATE PROCEDURE [dbo].[GetUserLayersByBuildStatus](
-@userObjectId UNIQUEIDENTIFIER,
-@BuildStatusId TINYINT
+﻿CREATE PROCEDURE [dbo].[GetUserLayers](
+@userObjectId UNIQUEIDENTIFIER
 )
 AS
 
 SELECT ul.UserLayerId, ul.[LayerId], b.LicenseId, b.ResolutionId, l.[LayerName] FROM [dbo].[UserLayer] ul 
 JOIN [dbo].[Layer] l ON ul.LayerId = l.LayerId 
 JOIN [dbo].[Build] b ON ul.BuildId = b.BuildId
-WHERE ul.UserObjectId = @userObjectId AND b.BuildStatusId = @BuildStatusId
+WHERE ul.UserObjectId = @userObjectId
