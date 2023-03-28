c61109abf0a6e7235d1bf1431177b618dc7993a0
seankennett <seankennettwork@gmail.com>
2023-03-28 23:31:03 +0100

    try again

--- VideoDatabase/Script.PostDeployment.sql
+++ VideoDatabase/Script.PostDeployment.sql
@@ -55,9 +55,9 @@ GRANT CONNECT TO [musicvideobuilder]
 GRANT EXECUTE TO [musicvideobuilder]
 END
 
-IF NOT EXISTS (SELECT [name] FROM sys.database_principals WHERE [name] = 'seankennettwork_gmail.com')
+IF NOT EXISTS (SELECT [name] FROM sys.database_principals WHERE [name] = 'seankennettwork@gmail.com')
 BEGIN
-CREATE USER [seankennettwork_gmail.com#EXT#@musicvideobuilder.onmicrosoft.com] FROM EXTERNAL PROVIDER
+CREATE USER [seankennettwork@gmail.com] FROM EXTERNAL PROVIDER
 
-EXEC sp_addrolemember N'db_owner', N'seankennettwork_gmail.com'
+EXEC sp_addrolemember N'db_owner', N'seankennettwork@gmail.com'
 END
