c17dee5509efd9b1ae61436af99c2912a4e0fcaa
seankennett <seankennettwork@gmail.com>
2023-04-04 13:34:04 +0100

    Split out add user script for local development

---
+++ AddUsers.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (SELECT [name] FROM sys.database_principals WHERE [name] = 'musicvideobuilder')
+BEGIN
+CREATE USER [musicvideobuilder] FROM EXTERNAL PROVIDER
+
+GRANT CONNECT TO [musicvideobuilder]
+
+GRANT EXECUTE TO [musicvideobuilder]
+END
+
+IF NOT EXISTS (SELECT [name] FROM sys.database_principals WHERE [name] = 'seankennettwork@gmail.com')
+BEGIN
+CREATE USER [seankennettwork@gmail.com] FROM EXTERNAL PROVIDER
+
+EXEC sp_addrolemember N'db_owner', N'seankennettwork@gmail.com'
+END
