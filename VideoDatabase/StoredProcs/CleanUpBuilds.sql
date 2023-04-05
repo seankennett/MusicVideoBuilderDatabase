e54ba2aa438abf56403397add0f4847ae4963c7b
seankennett <seankennettwork@gmail.com>
2023-04-05 18:03:45 +0100

    fix for cleanup proc to clear pendinguserlayers

--- VideoDatabase/StoredProcs/CleanUpBuilds.sql
+++ VideoDatabase/StoredProcs/CleanUpBuilds.sql
@@ -1,4 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[CleanUpBuilds]
 AS
+
+DECLARE @SevenDaysAgo SMALLDATETIME = DATEADD(DAY, -7, GETUTCDATE())
+
+DELETE FROM [dbo].[PendingUserLayer] WHERE BuildId IN (SELECT BuildId FROM [dbo].[Build] WHERE DateUpdated < @SevenDaysAgo)
 	
-DELETE FROM [dbo].[Build] WHERE DateUpdated < DATEADD(DAY, -7, GETUTCDATE())
+DELETE FROM [dbo].[Build] WHERE DateUpdated < @SevenDaysAgo
