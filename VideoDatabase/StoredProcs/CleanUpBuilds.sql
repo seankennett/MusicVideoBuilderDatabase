576e451425884e42d5338815d1ecb574c05b47a0
seankennett <seankennettwork@gmail.com>
2023-10-09 21:42:02 +0100

    Fixed broken proc

--- VideoDatabase/StoredProcs/CleanUpBuilds.sql
+++ VideoDatabase/StoredProcs/CleanUpBuilds.sql
@@ -3,6 +3,6 @@ AS
 
 DECLARE @SevenDaysAgo SMALLDATETIME = DATEADD(DAY, -7, GETUTCDATE())
 
-DELETE FROM [dbo].[PendingUserDisplayLayer] WHERE BuildId IN (SELECT BuildId FROM [dbo].[Build] WHERE DateUpdated < @SevenDaysAgo)
+DELETE FROM [dbo].[PendingUserCollection] WHERE BuildId IN (SELECT BuildId FROM [dbo].[Build] WHERE DateUpdated < @SevenDaysAgo)
 	
 DELETE FROM [dbo].[Build] WHERE DateUpdated < @SevenDaysAgo
