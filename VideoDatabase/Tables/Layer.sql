a3e045a4c4cb66a31705a692b51b2fed9ca4aa78
seankennett <seankennettwork@gmail.com>
2023-10-17 23:05:38 +0100

    Removing defaults as database now deployed

--- VideoDatabase/Tables/Layer.sql
+++ VideoDatabase/Tables/Layer.sql
@@ -4,6 +4,6 @@
 	[DisplayLayerId] UNIQUEIDENTIFIER NOT NULL,
 	[DateCreated] DATETIME2 NOT NULL,
 	[DefaultColour] CHAR(6) NOT NULL,
-	[IsOverlay] BIT NOT NULL CONSTRAINT DF_Layer_IsOverlay DEFAULT 0,
+	[IsOverlay] BIT NOT NULL,
 	CONSTRAINT FK_Layer_DisplayLayer FOREIGN KEY ([DisplayLayerId]) REFERENCES [dbo].[DisplayLayer] ([DisplayLayerId])
 )
