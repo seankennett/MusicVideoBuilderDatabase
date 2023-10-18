49a8804a0f40eafdfbf86f9d0e1cd9cade13dcba
seankennett <seankennettwork@gmail.com>
2023-10-18 15:56:48 +0100

    Changing video calls to only return relevent user information

--- VideoDatabase/StoredProcs/GetVideos.sql
+++ VideoDatabase/StoredProcs/GetVideos.sql
@@ -6,15 +6,6 @@ AS
 SELECT VideoId, BPM, VideoDelayMilliseconds, DateUpdated, FormatId, VideoName FROM [Video] 
 WHERE UserObjectId = @userObjectId
 
-SELECT vc.VideoId, vc.ClipId, vc.[Order], c.ClipName, c.BackgroundColour, c.BeatLength, c.StartingBeat FROM [VideoClips] vc
+SELECT vc.VideoId, vc.ClipId, vc.[Order] FROM [VideoClips] vc
 JOIN [dbo].[Clip] c ON vc.ClipId = c.[ClipId]
-WHERE c.UserObjectId = @userObjectId
-
-SELECT cd.ClipId, cd.DisplayLayerId, cd.[Reverse], cd.[Order], cd.ClipDisplayLayerId FROM [dbo].[ClipDisplayLayers] cd
-JOIN [dbo].[Clip] c ON c.ClipId = cd.ClipId
-WHERE c.UserObjectId = @userObjectId
-
-SELECT l.LayerId, l.ColourOverride, l.ClipDisplayLayerId FROM [dbo].[LayerClipDisplayLayers] l
-JOIN [dbo].[ClipDisplayLayers] cd ON cd.ClipDisplayLayerId = l.ClipDisplayLayerId
-JOIN [dbo].[Clip] c ON c.ClipId = cd.ClipId
 WHERE c.UserObjectId = @userObjectId
