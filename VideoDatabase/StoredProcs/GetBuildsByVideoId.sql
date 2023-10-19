a4c4f52f3042b90332ccb89e693b4858068c789f
seankennett <seankennettwork@gmail.com>
2023-10-19 21:33:22 +0100

    Added extra procs for better performance

---
+++ VideoDatabase/StoredProcs/GetBuildsByVideoId.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetBuildsByVideoId](
+@userObjectId UNIQUEIDENTIFIER,
+@VideoId INTEGER
+)
+AS
+
+SELECT BuildId, BuildStatusId, LicenseId, ResolutionId, VideoId, VideoName, PaymentIntentId, FormatId, HasAudio, DateUpdated  FROM [dbo].[Build] WHERE UserObjectId = @userObjectId AND VideoId = @VideoId
---
+++ VideoDatabase/StoredProcs/GetClipsByVideoId.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[GetClipsByVideoId](
+@userObjectId UNIQUEIDENTIFIER,
+@VideoId INTEGER
+)
+AS
+
+SELECT DISTINCT ClipId 
+INTO #Clips
+FROM VideoClips
+WHERE VideoId = @VideoId
+
+SELECT ClipId, ClipName, BackgroundColour, BeatLength, StartingBeat  FROM [Clip] 
+WHERE UserObjectId = @userObjectId AND ClipId IN (SELECT ClipId FROM #Clips)
+
+SELECT cd.ClipId, cd.DisplayLayerId, cd.[Reverse], cd.[Order], cd.ClipDisplayLayerId FROM [dbo].[ClipDisplayLayers] cd
+JOIN [dbo].[Clip] c ON c.ClipId = cd.ClipId
+WHERE c.UserObjectId = @userObjectId AND c.ClipId IN (SELECT ClipId FROM #Clips)
+
+SELECT l.LayerId, l.ColourOverride, l.ClipDisplayLayerId FROM [dbo].[LayerClipDisplayLayers] l
+JOIN [dbo].[ClipDisplayLayers] cd ON cd.ClipDisplayLayerId = l.ClipDisplayLayerId
+JOIN [dbo].[Clip] c ON c.ClipId = cd.ClipId
+WHERE c.UserObjectId = @userObjectId AND c.ClipId IN (SELECT ClipId FROM #Clips)
