5348839c9d9019d90aeca5c824d06114d1bd597e
seankennett <seankennettwork@gmail.com>
2023-10-20 19:31:51 +0100

    Removing defaults on new column

--- VideoDatabase/Tables/Layer.sql
+++ VideoDatabase/Tables/Layer.sql
@@ -5,6 +5,6 @@
 	[DateCreated] DATETIME2 NOT NULL,
 	[DefaultColour] CHAR(6) NOT NULL,
 	[IsOverlay] BIT NOT NULL,
-	[Order] TINYINT NOT NULL DEFAULT 0,
+	[Order] TINYINT NOT NULL,
 	CONSTRAINT FK_Layer_DisplayLayer FOREIGN KEY ([DisplayLayerId]) REFERENCES [dbo].[DisplayLayer] ([DisplayLayerId])
 )
