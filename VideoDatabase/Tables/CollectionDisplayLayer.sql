fc9da94b780cf7adf0f4f51b2f6c3f2552b3680c
seankennett <seankennettwork@gmail.com>
2023-10-23 21:02:58 +0100

    switched up keys for render uploader but is better design regardless

--- VideoDatabase/Tables/CollectionDisplayLayer.sql
+++ VideoDatabase/Tables/CollectionDisplayLayer.sql
@@ -2,6 +2,7 @@
 (
 	[CollectionId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
 	[DisplayLayerId] UNIQUEIDENTIFIER NOT NULL,
+	CONSTRAINT UQ_CollectionDisplayLayer_DisplayLayer UNIQUE ([DisplayLayerId]),
 	CONSTRAINT FK_CollectionDisplayLayer_Collection FOREIGN KEY ([CollectionId]) REFERENCES [dbo].[Collection] ([CollectionId]),
 	CONSTRAINT FK_CollectionDisplayLayer_DisplayLayer FOREIGN KEY ([DisplayLayerId]) REFERENCES [dbo].[DisplayLayer] ([DisplayLayerId])
 )
--- VideoDatabase/Tables/LayerCollectionDisplayLayers.sql
+++ VideoDatabase/Tables/LayerCollectionDisplayLayers.sql
@@ -3,6 +3,6 @@
 	[LayerId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
 	[DisplayLayerId] UNIQUEIDENTIFIER NOT NULL,
 	[Colour] CHAR(6) NOT NULL,
-	CONSTRAINT FK_LayerCollectionDisplayLayers_DisplayLayers FOREIGN KEY ([DisplayLayerId]) REFERENCES [dbo].[DisplayLayer] ([DisplayLayerId]),
+	CONSTRAINT FK_LayerCollectionDisplayLayers_CollectionDisplayLayers FOREIGN KEY ([DisplayLayerId]) REFERENCES [dbo].[CollectionDisplayLayer] ([DisplayLayerId]),
 	CONSTRAINT FK_LayerCollectionDisplayLayers_Layer FOREIGN KEY ([LayerId]) REFERENCES [dbo].[Layer] ([LayerId])
 )
